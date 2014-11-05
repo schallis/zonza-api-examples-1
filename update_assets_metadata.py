c642ef3923692e837bfe04d94ceab97352dca964
Steven Challis <steven.challis@hogarthww.com>
2014-11-04 19:18:14 -0500

    Multi threaded updates!`:wq`

--- update_assets_metadata.py
+++ update_assets_metadata.py
@@ -3,14 +3,15 @@ import os
 import requests
 import json
 import sys
+import time
 
 from pprint import pprint
 
 PAGE_SIZE = 100
 SITE = 'demo.zonza.tv'
-FIELD = 'demo_brand'
-OLD = 'old'
-NEW = 'new'
+FIELD = 'media_type'
+OLD = 'OLD'
+NEW = 'NEW'
 
 url = 'http://api.zonza.tv:8080/v0/'
 
@@ -27,33 +28,57 @@ print 'searching...'
 headers = {'content-type': 'application/json'}
 headers.update(auth)
 
+# Public API requests are ratelimited to 10rps
+
 items = []
 hits = 0
 page = 1
-pages = 2
-while page < pages:
+pages = 1
+while page <= pages:
     response = requests.get(
-        '{}item?zonza_site={}&{}={}&__page={}&__page_size={}'.format(
+        '{}item?zonza_site={}&{}={}&__page={}&__page_size={}&zonza_supporting_file_master='.format(
             url, SITE, FIELD, OLD, page, PAGE_SIZE),
         headers=headers)
     json_response = json.loads(response.content)
     hits = int(json_response['hits'])
-    pages = 1 + (hits // PAGE_SIZE)
+
     items.extend(json_response['item'])
+    print "Page {}, {} item(s) found now".format(page, len(items))
+
+    pages = 1 + (hits // PAGE_SIZE)
+    page += 1
 
 print "About to update", hits, "asset(s)..."
 print "'{}' will be updated to '{}' for field '{}'".format(OLD, NEW, FIELD)
 
+time.sleep(3)
 
 # Update after retieving all items to avoid modifying the paginated results
 # whilst iterating
 
-for item in items:
+# Multiprocessing! Warning: Vidispine facet indexes seem to lag
+
+def update_field(item):
     item_url = item['url'] + '/metadata'
-    print item_url
     new_data = {FIELD: NEW}
     update_response = requests.post(
             item_url,
             data=json.dumps(new_data),
             headers=headers)
-    print update_response
+    print update_response, item_url
+
+from multiprocessing import Pool
+from multiprocessing.dummy import Pool as ThreadPool
+pool = ThreadPool(10) # Sets the pool size
+results = pool.map(update_field, items)
+pool.close()
+pool.join()
+
+#for number, item in enumerate(items):
+    #item_url = item['url'] + '/metadata'
+    #new_data = {FIELD: NEW}
+    #update_response = requests.post(
+            #item_url,
+            #data=json.dumps(new_data),
+            #headers=headers)
+    #print number, update_response, item_url
